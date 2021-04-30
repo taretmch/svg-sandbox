b7efb8dd6dd6177c0799a1e30b76dceb84c64c9f
taretmch <tomochuri25@gmail.com>
2021-05-01 01:17:12 +0900

    Add configuration for building scss

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+// Plugin: For extracting css file
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+/**
+ * Configuration for webpack
+ */
+module.exports = {
+  module: {
+    rules: [
+      // SCSS File loader
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ]
+      }
+    ]
+  },
+  plugins: [ new MiniCssExtractPlugin() ]
+}
